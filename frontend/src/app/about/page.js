ea1e9d5a63e578636ccb331db1d502ec3b43d8e3
Exorz <boardsarena@gmail.com>
2024-11-14 11:46:27 +0100

    Update page.js

--- frontend/src/app/about/page.js
+++ frontend/src/app/about/page.js
@@ -14,7 +14,7 @@ export default function AboutPage() {
           At BoardArena, we believe in creating a community where players of all skill levels can come together, share their passion for games, and compete in a friendly, supportive environment. From classic board games to new favorites, our game lobbies provide the perfect setting for hours of fun, strategy, and camaraderie.
         </p>
         <p className={styles.content}>
-          Join us today, earn your crowns, and see if you have what it takes to claim the best crown in the arena!
+          Join us today, earn your crowns, and see if you have what it takes to claim the best crown in the arena! g
         </p> 
       </div>
     </main>
