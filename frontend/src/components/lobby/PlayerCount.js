9c0747e4ef0d1fd6e8fe7b0867f21db75a672303
Exorz <boardsarena@gmail.com>
2024-11-15 19:07:01 +0100

    Update PlayerCount.js

--- frontend/src/components/lobby/PlayerCount.js
+++ frontend/src/components/lobby/PlayerCount.js
@@ -1,6 +1,24 @@
-// \frontend\src\components\lobby\PlayerCount.js
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 import styles from '../../styles/Lobby.module.css';
 
-export default function PlayerCount() {
-  return <p className={styles.onlineCount}>Online: 1</p>;
+const socket = io('http://90.143.144.169:3000');
+
+export default function PlayerCount({ game }) {
+  const [playerCount, setPlayerCount] = useState(1);
+
+  useEffect(() => {
+    const token = localStorage.getItem('authToken');
+    socket.emit('joinLobby', { lobbyId: game, token });
+
+    socket.on('updatePlayerCount', (count) => {
+      setPlayerCount(count);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [game]);
+
+  return <p className={styles.onlineCount}>Online: {playerCount}</p>;
 }
