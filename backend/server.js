60906bb0980fb8b6ff365186954a8e58b0a58958
Exorz <boardsarena@gmail.com>
2024-11-16 10:57:05 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,15 +30,28 @@ app.get('/', (req, res) => {
 });
 
 // Load Socket.IO modules
+console.log('Initializing Socket.IO...');
 generalSocket(io);
 chatSocket(io);
+console.log('Socket.IO initialized.');
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://johannessonandree:Fiskbulle1a@db.bo2i6.mongodb.net/db?retryWrites=true&w=majority')
+mongoose
+  .connect('mongodb+srv://johannessonandree:Fiskbulle1a@db.bo2i6.mongodb.net/db?retryWrites=true&w=majority')
   .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Could not connect to MongoDB', err));
+  .catch((err) => console.error('Could not connect to MongoDB:', err.message));
 
+// Log if server starts correctly
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// Handle unhandled errors in the process
+process.on('uncaughtException', (err) => {
+  console.error('There was an uncaught error:', err.message);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled promise rejection:', reason);
 });
