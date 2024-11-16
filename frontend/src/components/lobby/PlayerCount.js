274bed5b1c857ec74650f98e96c2b2c8a65052a3
Exorz <boardsarena@gmail.com>
2024-11-16 10:53:04 +0100

    Update PlayerCount.js

--- frontend/src/components/lobby/PlayerCount.js
+++ frontend/src/components/lobby/PlayerCount.js
@@ -1,18 +1,16 @@
 import { useEffect, useState } from "react";
-import { socket } from "../../socketManager"; // Hantera socket i socketManager.js
+import { socket } from "../../socketManager";
 import styles from "../../styles/Lobby.module.css";
 
 export default function PlayerCount({ game }) {
   const [playerCount, setPlayerCount] = useState(1);
 
   useEffect(() => {
-    // Lyssna på uppdateringar av spelarantal
     socket.on("updatePlayerCount", (count) => {
-      console.log("Received updated player count:", count);
+      console.log("Player count updated to:", count);
       setPlayerCount(count);
     });
 
-    // Rensa lyssnare vid avmontering
     return () => {
       console.log("Removing updatePlayerCount listener");
       socket.off("updatePlayerCount");
