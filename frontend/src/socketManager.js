ba0de08743984a78cad049ec9f0dda61327de772
Exorz <boardsarena@gmail.com>
2024-11-16 10:56:05 +0100

    Update socketManager.js

--- frontend/src/socketManager.js
+++ frontend/src/socketManager.js
@@ -1,18 +1,22 @@
 import { io } from 'socket.io-client';
 
+// Skapa en Socket.IO-klientinstans
 const socket = io('http://90.143.144.169:3000', {
   transports: ['websocket'],
   autoConnect: false,
 });
 
+// Hantera anslutning
 socket.on('connect', () => {
   console.log(`Connected to server with socket ID: ${socket.id}`);
 });
 
+// Hantera frånkoppling
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
 });
 
+// Funktion för att ansluta till servern med token
 const connectWithToken = () => {
   const token = localStorage.getItem('authToken');
   console.log('Attempting to connect with token:', token);
@@ -22,8 +26,13 @@ const connectWithToken = () => {
   }
   socket.auth = { token };
   socket.connect();
+
+  socket.on('connect_error', (err) => {
+    console.error('Connection error:', err.message);
+  });
 };
 
+// Funktion för att gå med i en lobby
 const joinLobby = (lobbyId) => {
   console.log(`Joining lobby: ${lobbyId}`);
   const token = localStorage.getItem('authToken');
